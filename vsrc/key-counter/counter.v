a39a5b08cc24ccfd40ae629e17f922cf2b26a6e2
zian <251180057@smail.nju.edu.cn>
2026-08-16 20:12:28 +0800

    finish counter.v

---
+++ vsrc/key-counter/counter.v
@@ -0,0 +1,26 @@
+module counter(
+    input clk,
+    input reset,
+    input plus,
+    input minus,
+    input setzero,
+    input setnum,
+    output reg [5:0]num
+);
+wire [3:0]signal;
+assign signal={plus,minus,setzero,setnum};
+always @(posedge clk or negedge reset)begin
+    if(!reset)begin
+        num<=0;
+    end
+    else begin
+        case(signal)
+            4'b1000:if(num<60)num<=num+1;
+            4'b0100:if(num>0)num<=num-1;
+            4'b0010:num<=0;
+            4'b0001:num<=23;
+            default:num<=num;
+        endcase
+    end
+end
+endmodule
